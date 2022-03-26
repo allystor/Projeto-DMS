53066a2edb71d8afcf5cebfd584a8853eb00e56a
allystor <alvarojoao17@gmail.com>
2022-03-26 20:02:03 -0300

    update commit

--- Competição/app.py
+++ Competição/app.py
@@ -1,4 +1,4 @@
-
+from disputa import Adcionando
 from flask import Flask, render_template
 
 app = Flask(__name__)
@@ -6,6 +6,10 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     return render_template('index.html')
+
+@app.route('/adicionar')
+def adicionar():
+    return render_template('adicionar_times.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
---
+++ Competição/disputa.py
@@ -0,0 +1,13 @@
+
+class Adcionando:
+    def __init__(self,id, time, jogador):
+        self.__id = id
+        self.__time = time
+        self.__jogador = jogador
+        
+    def getTime(self):
+        return self.__id
+    def getTime(self):
+        return self.__time
+    def getJogador(self):
+        return self.__jogador
